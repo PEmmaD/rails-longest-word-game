0a1fbe6c420d73a00c4bfb3b8d3778b1c0fb6dd4
Paule Emma DULOIR <pauleemma.duloir@gmail.com>
2017-02-06 19:20:52 +0100

    ex 2 almost done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  root 'words#game'
+  get 'game' =>  'words#game'
+  get 'score' => 'words#score'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
+
+
+
+
+
